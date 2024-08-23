495e026e131bb9abd907dc009bfb2dc5d6494008
batsokin93 <152905867+valensia1853@users.noreply.github.com>
2024-08-23 22:27:17 +0300

    Create views.py

---
+++ views.py
@@ -0,0 +1,31 @@
+import datetime
+import os
+
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+
+
+def home_view(request):
+    template_name = 'app/home.html'
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }
+    
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    current_time = datetime.datetime.now().time()
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    workdir = os.listdir(path='.')
+    msg = f'Список файлов в директории: {workdir}'
+    return HttpResponse(msg)
